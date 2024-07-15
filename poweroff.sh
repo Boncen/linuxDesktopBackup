04bcbbaa44d6535ef4ccbcf04cd8eb6f43a56da3
boncen <boncen@outlook.com>
2024-07-15 23:47:05 +0800

    add skip feature

--- poweroff.sh
+++ poweroff.sh
@@ -4,25 +4,34 @@ gitNotesPaths=(
 	"/home/boncen/notes"
 )
 githubReposPath="/home/boncen/github"
-
+input="y"
 for path in $gitNotesPaths
 do
 	cd $path
 	if [[ ! -z $(git status -s) ]];then
-		echo $path + '下的内容还未提交,关机终止。'
-		exit 1	
+		echo "$path 下的内容还未提交,关机终止"
+		read -p "Skip?(Y/n)" input
+		if [[ $input == "y" || -z $input ]];then
+			echo "skip $path"
+		else
+			exit 1
+		fi
 	fi
-	echo $path
 done
 
 for path in $(ls $githubReposPath )
 do
 	cd "$githubReposPath/$path"
 	if [[ ! -z $(git status -s)  ]];then
-		echo "$githubReposPath/$path" + '内容未提交，关机终止。'
-		exit 1
+		echo "$githubReposPath/$path 内容未提交，关机终止"
+		read -p "Skip?(Y/n)" input
+		if [[ $input == "y" || -z $input ]];then
+			echo "skip $path"
+		else
+			exit 1
+		fi
 	fi
 done
 
-echo "准备关机～"
-sudo poweroff
+echo "Ready for poweroff ～"
+sync && sync && sync && sudo poweroff
